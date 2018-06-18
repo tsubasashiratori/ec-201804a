3d225f25cace971cc2d8f7e947a673ae051744d1
yuta.kitazawa <yuta.kitazawa@rakus.co.jp>
2018-06-18 14:43:49 +0900

    git init       git add README.md       git commit -m "first commit"       git remote add origin https://172.16.0.14/gitbucket/git/yuta.kitazawa/ec-201804a.git       git push -u origin master

---
+++ src/main/java/jp/co/rakus/ec201804a/Ec201804aApplication.java
@@ -0,0 +1,12 @@
+package jp.co.rakus.ec201804a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ec201804aApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ec201804aApplication.class, args);
+	}
+}
---
+++ src/test/java/jp/co/rakus/ec201804a/Ec201804aApplicationTests.java
@@ -0,0 +1,16 @@
+package jp.co.rakus.ec201804a;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Ec201804aApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
