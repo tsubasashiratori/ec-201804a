de14882d7457317b44e4cba9b6b1818e7a81732f
shunta.nakamura <shunta.nakamura@rakus.co.jp>
2018-06-20 16:40:55 +0900

    16管理者削除の新規作成

---
+++ src/main/java/jp/co/rakus/ec201804a/admin/delete/ViewAllAndSearchItemController.java
@@ -0,0 +1,15 @@
+package jp.co.rakus.ec201804a.admin.delete;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 
+ * @author shunta.nakamura
+ *
+ */
+@Controller
+@RequestMapping("/admin/ViewAllAndSearchItemController")
+public class ViewAllAndSearchItemController {
+
+}
--- src/test/java/jp/co/rakus/ec201804a/Ec201804aApplicationTests.java
+++ src/test/java/jp/co/rakus/ec201804a/Ec201804aApplicationTests.java
@@ -2,16 +2,32 @@ package jp.co.rakus.ec201804a;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import jp.co.rakus.ec201804a.admin.delete.EditItemRepository;
+import jp.co.rakus.ec201804a.common.domain.Item;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class Ec201804aApplicationTests {
 
+	@Autowired
+	private EditItemRepository repository;
+
 	@Test
 	public void contextLoads() {
-		
+		try {
+			Long num = (long) 2;
+			Item item = repository.load(num);
+			System.out.println(item);
+			item.setDeleted(false);
+			repository.save(item);
+			Item itemNew = repository.load(num);
+			System.out.println(itemNew);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
 	}
 }
-
