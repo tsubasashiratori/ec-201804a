3eb08d7e7550c53e7a6614ab43b2dea75aa51a09
shunta.nakamura <shunta.nakamura@rakus.co.jp>
2018-06-20 12:00:32 +0900

    4商品一覧表示 エラー文の表示

--- src/main/java/jp/co/rakus/ec201804a/user/ViewAllAndsearch4/controller/ViewAlItemsAndSearchItemController.java
+++ src/main/java/jp/co/rakus/ec201804a/user/ViewAllAndsearch4/controller/ViewAlItemsAndSearchItemController.java
@@ -2,45 +2,64 @@ package jp.co.rakus.ec201804a.user.ViewAllAndsearch4.controller;
 
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import jp.co.rakus.ec201804a.common.domain.Item;
-import jp.co.rakus.ec201804a.user.viewAllAndsearch4.repository.ViewAllItemsRepository;
+import jp.co.rakus.ec201804a.user.ViewAllAndsearch4.form.ViewAllItemsAndSearchItemForm;
+import jp.co.rakus.ec201804a.user.ViewAllAndsearch4.repository.ViewAllItemsRepository;
 
+/**
+ * @author shunta.nakamura
+ *
+ */
 @Controller
 @RequestMapping("/ViewAllItemsAndSearchItem")
 public class ViewAlItemsAndSearchItemController {
 
-//	@ModelAttribute
-//	public ViewAllItemsAndSearchItemForm setUpForm() {
-//		return new ViewAllItemsAndSearchItemForm();
-//	}
 	@Autowired
 	private ViewAllItemsRepository repository;
 
+	@ModelAttribute
+	public ViewAllItemsAndSearchItemForm setUpForm() {
+		return new ViewAllItemsAndSearchItemForm();
+	}
+
 	@RequestMapping("/findAllNotDeleted")
-	public String findAllNotDeleted(HttpServletRequest request) {
-		
+	public String findAllNotDeleted(Model model) {
+
 		List<Item> itemList = repository.findAllNotDeleted();
-		
-		request.setAttribute("itemList", itemList);
-		
+
+		model.addAttribute("itemList", itemList);
+
 		return "/user/viewShoppingCart";
 
 	}
-	
+
 	@RequestMapping("/findByNameNotDeleted")
-	public String findByNameNotDeleted(@RequestParam ("name") String name, HttpServletRequest request) {
+	public String findByNameNotDeleted(@Validated ViewAllItemsAndSearchItemForm viewAllItemsAndSearchForm, BindingResult result, Model model) {
+
+		String name = viewAllItemsAndSearchForm.getName();
 		
 		List<Item> itemList = repository.findByNameNotDeleted(name);
+
 		
-		request.setAttribute("itemList", itemList);
+		if(itemList.size() == 0) {
+			
+			result.rejectValue("name","", "該当する商品がありません");
+			System.out.println("test");
+		}
+		if(result.hasErrors()) {
+			return "/user/viewShoppingCart";
+		}
 		
+		model.addAttribute("itemList", itemList);
+
 		return "/user/viewShoppingCart";
 	}
 }
