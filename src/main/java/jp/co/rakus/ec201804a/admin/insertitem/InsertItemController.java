9efc3aa9dc641753f3295b7e15572f0586e8a111
shiratoritsubasa <tsubasa.shiratori@rakus.co.jp>
2018-06-20 17:17:03 +0900

    商品登録画面の作成

---
+++ src/main/java/jp/co/rakus/ec201804a/admin/insertitem/InsertItemController.java
@@ -0,0 +1,66 @@
+package jp.co.rakus.ec201804a.admin.insertitem;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jp.co.rakus.ec201804a.common.domain.Item;
+import jp.co.rakus.ec201804a.common.repository.ItemRepository;
+
+/**
+ * 商品登録処理を行うコントローラー.
+ * 
+ * @author tsubasa.shiratori
+ */
+@Controller
+@Transactional
+@RequestMapping(value="/admin/insertItem")
+public class InsertItemController {
+	
+	@Autowired
+	private ItemRepository itemRepository;
+	
+	/**
+	 * フォームの初期化
+	 * @return 初期かされたフォーム
+	 */
+	@ModelAttribute
+	public InsertItemForm setUpForm() {
+		return new InsertItemForm();
+	}
+	
+	/**
+	 * 商品登録画面表示.
+	 * 
+	 * @return 商品登録画面
+	 */
+	@RequestMapping(value="/")
+	public String viewInsertItem() {
+		return "admin/insertItem";
+	}
+	
+	/**
+	 * 商品登録処理.
+	 * 
+	 * @param form フォーム
+	 * @param result リザルト
+	 * @return 商品一覧画面
+	 */
+	@RequestMapping(value="/insert")
+	public String insertItem(@Validated InsertItemForm form, BindingResult result) {
+		if (result.hasErrors()) {			
+			return viewInsertItem();
+		}
+		Item item = new Item();
+		BeanUtils.copyProperties(form, item);
+		System.out.println(item);
+		itemRepository.insertItem(item);
+		return "redirect:/admin/adminAllandSearch/adminFindAll";
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/views/admin/insertItem.jsp
@@ -0,0 +1,40 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"%>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form"%>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title>商品登録画面</title>
+</head>
+<%@ include file="../common/adminHeader.jsp"%>
+<body>
+	<div align="center">
+		<h2>商品登録画面</h2>
+		<div>新規で登録したい商品の情報を入力してください。</div>
+		<br>
+	<form:errors path="ItemForm*."/>
+		<form:form modelAttribute="insertItemForm" action="${pageContext.request.contextPath}/admin/insertItem/insert" method="post" onsubmit="return check()"
+			onreset="return kakunin()">
+			商品名：<form:input path="name"/><br>
+			<p><small><form:errors path="name" cssStyle="color:red" /></small></p>
+			<br>
+			価格 ：<form:input path="price"/><br>
+			<p><small><form:errors path="price" cssStyle="color:red" /></small></p>
+			<br>
+			説明 ：<form:textarea path="description" cols="40" rows="5"/><br>
+			<p><small><form:errors path="description" cssStyle="color:red" /></small></p>
+			<br>
+			画像 ：<input type="file" name="imagePath"
+			accept="コンテンツタイプ（Content-Type）"><br>
+			<div>
+				<font size="2" color="red">※ファイルはjpgファイルを指定してください</font>
+			</div>
+			<p><small><form:errors path="imagePath" cssStyle="color:red" /></small></p>
+			<br>
+			<form:checkbox path="deleted"/>利用者商品一覧に表示しない<br>
+			<input type="submit" value="登録"><br>
+		</form:form>
+		<br> <a href="${pageContext.request.contextPath}/administerMenu">管理者メニュー画面に戻る</a>
+	</div>
+</body>
+</html>
