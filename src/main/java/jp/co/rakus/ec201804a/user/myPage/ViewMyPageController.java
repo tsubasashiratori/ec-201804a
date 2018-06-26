cee0c1d1b1438c96c9cf7d8289f83feb477c3498
yuta.kitazawa <yuta.kitazawa@rakus.co.jp>
2018-06-26 16:13:19 +0900

    利用者の購入履歴を表示する機能の簡単な実装

--- src/main/java/jp/co/rakus/ec201804a/user/myPage/ViewMyPageController.java
+++ src/main/java/jp/co/rakus/ec201804a/user/myPage/ViewMyPageController.java
@@ -3,9 +3,19 @@ package jp.co.rakus.ec201804a.user.myPage;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * 利用者のマイページを表示するコントローラ.
+ * 
+ * @author yuta.kitazawa
+ */
 @Controller
 @RequestMapping(value = "/user")
 public class ViewMyPageController {
+	/**
+	 * マイページを表示する.
+	 * 
+	 * @return マイページ
+	 */
 	@RequestMapping(value = "/myPage")
 	public String myPage() {
 		return "user/myPage";
